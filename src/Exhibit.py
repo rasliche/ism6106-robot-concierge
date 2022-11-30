a81f259db5a74cb798a828fb667888c73f2654f2
Eric Raslich <rasliche@gmail.com>
2022-11-30 14:17:18 -0500

    Exhibit and Panel classes to delineate ares of interest within a visitor center

---
+++ src/Exhibit.py
@@ -0,0 +1,7 @@
+class Exhibit():
+    def __init__(self, type="wall", location="main") -> None:
+        self._type = type
+        self._location = location
+
+    def visitorCount(self):
+        pass
---
+++ src/Panel.py
@@ -0,0 +1,12 @@
+from Exhibit import Exhibit
+
+class Panel(Exhibit):
+    def __init__(self, name, type="wall", location="main") -> None:
+        super().__init__(type, location)
+        self._name = name
+
+    def count():
+        pass
+
+    def stayDuration():
+        pass
