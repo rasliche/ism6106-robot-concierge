6bd535977fdb9fbf2c26a37c2259ee78f0785808
Eric Raslich <rasliche@gmail.com>
2022-11-30 14:24:25 -0500

    Timer class for tracking and reporting back time that an exhibit is being interacted with

---
+++ src/Timer.py
@@ -0,0 +1,21 @@
+from time import time
+now = time()
+
+print(time()-now)
+
+class Timer():
+    def __init__(self) -> None:
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def report(self):
+        pass
+
+    def reset(self):
+        pass
+
