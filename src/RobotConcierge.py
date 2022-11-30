63e8cd306c42148d1285d976d5c3b0f66c281573
Eric Raslich <rasliche@gmail.com>
2022-11-30 14:15:14 -0500

    main class that orchestrates the product

---
+++ src/RobotConcierge.py
@@ -0,0 +1,12 @@
+class RobotConcierge():
+    def __init__(self) -> None:
+        pass
+
+    def trackVisitor():
+        pass
+
+    def trackArea():
+        pass
+
+    def trackTime():
+        pass
