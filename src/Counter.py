f008c9fe686b6e80d96f5e97c1203a2f8270fa61
Eric Raslich <rasliche@gmail.com>
2022-11-30 14:16:44 -0500

    Counter class to track numbers of visitors

---
+++ src/Counter.py
@@ -0,0 +1,15 @@
+class Counter():
+    def __init__(self) -> None:
+        pass
+
+    def reset(self):
+        pass
+
+    def increment(self):
+        pass
+
+    def decrement(self):
+        pass
+
+    def report(self):
+        pass
